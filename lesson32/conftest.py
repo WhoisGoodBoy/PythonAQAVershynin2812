c4a01f8163f569189c3c909877396896128bff84
WhoisGoodBoy <veolvl@gmail.com>
2024-04-18 22:17:08 +0300

    lesson32 headless

--- lesson32/conftest.py
+++ lesson32/conftest.py
@@ -11,9 +11,8 @@ url = "https://petslike.ua/"
 @pytest.fixture
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch()
         page = browser.new_page()
-
         yield page
         page.close()
 
